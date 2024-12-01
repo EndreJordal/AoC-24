dac40aeb144b0dbb63572ac23c27cb09e9e3a4fc
Endre Jordal <endre.jordal@gmail.com>
2024-12-01 19:09:01 +0100

    day1

---
+++ Day1/day1.mjs
@@ -0,0 +1,28 @@
+import fs from "fs";
+
+const lines = fs.readFileSync("day1.txt", "utf8").split("\n");
+const left = [];
+const right = [];
+
+for (let line of lines) {
+  const nums = line.split("   ");
+  left.push(+nums[0]);
+  right.push(+nums[1]);
+}
+left.sort((a, b) => a - b);
+right.sort((a, b) => a - b);
+
+let totalDistance = 0;
+
+for (let i = 0; i < left.length; i++) {
+  totalDistance += Math.abs(left[i] - right[i]);
+}
+console.log(totalDistance);
+
+// Part 2
+let similarityScore = 0;
+
+for (let leftNum of left) {
+  similarityScore += leftNum * right.filter((e) => e === leftNum).length;
+}
+console.log(similarityScore);
