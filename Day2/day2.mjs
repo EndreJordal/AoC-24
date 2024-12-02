b6e8e8532d5ba0f8fe9322088405db2449f3f568
Endre Jordal <endre@jobloop.no>
2024-12-02 17:21:41 +0100

    day 2 in progress

---
+++ Day2/day2.mjs
@@ -0,0 +1,46 @@
+import fs from "fs";
+
+const lines = fs
+  .readFileSync("data.txt", "utf8")
+  .split("\n")
+  .filter((line) => line.trim() !== "");
+
+let unsafeReport = 0;
+let safeReport = 0;
+
+for (const line of lines) {
+  const report = line.split(" ").map(Number);
+  let ascend;
+  // console.log(report);
+  if (report[0] === report[1]) {
+    unsafeReport++;
+    continue;
+  }
+
+  ascend = report[0] < report[1];
+
+  let isUnsafe = false;
+  let errorToleranceUsed = false;
+  for (let i = 0; i < report.length - 1; i++) {
+    if (
+      (ascend && report[i] > report[i + 1]) ||
+      (!ascend && report[i] < report[i + 1]) ||
+      Math.abs(report[i] - report[i + 1]) > 3 ||
+      report[i] === report[i + 1]
+    ) {
+      if (!errorToleranceUsed) {
+        errorToleranceUsed = true;
+      } else {
+        unsafeReport++;
+        // console.log("unsafe: ", report);
+        isUnsafe = true;
+        break;
+      }
+    }
+  }
+  if (!isUnsafe) {
+    // console.log("safe: ", report);
+    safeReport++;
+  }
+}
+console.log(unsafeReport, safeReport);
